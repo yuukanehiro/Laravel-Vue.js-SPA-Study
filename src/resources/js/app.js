fc545c99fbc32db6d839e81c0a5f3bd4c2e02e28
bravesoft-kanehiroyuu <y.kanehiro@bravesoft.co.jp>
2020-11-24 01:49:55 +0900

    ・Header, Taskコンポーネント追加 ・dodker nginx defaultファイル修正 Laravelのパスに対応できるように

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,3 +1,7 @@
+import VueRouter from 'vue-router';
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -7,6 +11,20 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+    ]
+});
+
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,6 +38,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +48,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
